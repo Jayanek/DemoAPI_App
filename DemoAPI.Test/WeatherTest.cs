a5aceed865dc2a2bc0fabb2a47a498e3fba2933c
Jayanek <jayanekwije123@gmail.com>
2022-01-28 15:54:49 +0530

    Initial Commit

---
+++ DemoAPI.Test/WeatherTest.cs
@@ -0,0 +1,25 @@
+﻿using DemoAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DemoAPI.Test
+{
+    public class WeatherTest
+    {
+        [Fact]
+        public void WeatherGetSuccess()
+        {
+            var wc = new WeatherForecastController();
+
+            var actual = wc.Get();
+
+            var expected = "Hello World";
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
